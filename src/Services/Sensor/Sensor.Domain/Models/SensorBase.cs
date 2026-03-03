29778bfa9ff068b06473e985bfa488a22ed06912
Daniel Lehmann <139554573+lehmand@users.noreply.github.com>
2026-03-03 11:33:14 +0000

    changes to docker compose

--- src/Services/Sensor/Sensor.Domain/Models/SensorBase.cs
+++ src/Services/Sensor/Sensor.Domain/Models/SensorBase.cs
@@ -5,6 +5,7 @@ public class SensorBase
     public Guid Id { get; set; }
     public required string DeviceName { get; set; }
     public string? Room { get; set; }
+    public string? Location { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime LastUpdated { get; set; }
     public bool IsOnline { get; set; } = false;
