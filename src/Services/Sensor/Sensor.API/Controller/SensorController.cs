bbaebe7c2cd9c44838b7e6d6bc98f9a3b7a5457f
Daniel Lehmann <139554573+lehmand@users.noreply.github.com>
2026-04-21 11:44:40 +0000

    feature: added controller

---
+++ src/Services/Sensor/Sensor.API/Controller/SensorController.cs
@@ -0,0 +1,13 @@
+
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sensor.API.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class SensorController : ControllerBase
+{
+    
+}
